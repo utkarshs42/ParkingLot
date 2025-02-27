bc810d045798280ebcdd747fa68427efcf63e6af
Utkarsh Shukla <utkarshs9727@gmail.com>
2025-02-28 01:36:48 +0530

    Allotment Strategy added

--- src/main/java/strategies/RandomAllotmentStrategy.java
+++ src/main/java/strategies/RandomAllotmentStrategy.java
@@ -1,12 +1,18 @@
 package strategies;
 
-import models.ParkingLot;
-import models.ParkingSpot;
-import models.VehicleType;
+import models.*;
 
 public class RandomAllotmentStrategy implements AllotmentStrategy {
     @Override
     public ParkingSpot getParkingSpot(VehicleType vehicleType, ParkingLot parkingLot) {
+        for(ParkingFloor parkingFloor : parkingLot.getFloorList()){
+            for(ParkingSpot parkingSpot : parkingFloor.getParkingSpotList()){
+                if(parkingSpot.getVehicleType().equals(vehicleType) &&
+                        parkingSpot.getStatus() == ParkingSpotStatus.Empty ){
+                    return parkingSpot;
+                }
+            }
+        }
         return null;
     }
 }
